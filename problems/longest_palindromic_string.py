f5caa92ff14ec9ec006c55a2efc32408ddd39cc3
Devashish <dobriyaldevashish@gmail.com>
2026-01-21 23:27:29 +0530

    Longest palinfromic substring

---
+++ problems/longest_palindromic_string.py
@@ -0,0 +1,25 @@
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
+
+        start = 0
+        max_len = 1
+
+        def expand(left: int, right: int):
+            nonlocal start, max_len
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                curr_len = right - left + 1
+                if curr_len > max_len:
+                    start = left
+                    max_len = curr_len
+                left -= 1
+                right += 1
+
+        for i in range(len(s)):
+            # Odd-length palindrome
+            expand(i, i)
+
+            # Even-length palindrome
+            expand(i, i + 1)
+
+        return s[start:start + max_len]
